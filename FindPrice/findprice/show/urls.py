4f144c982251eff08869c3857b0b5da808f6d901
haydenphancs <haydenphancs@gmail.com>
2024-06-21 17:46:32 -0600

    Run
    Search and Delete

--- FindPrice/findprice/show/urls.py
+++ FindPrice/findprice/show/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
 from . import views
+from django.urls import path
+from .views import delete_all_data
 
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('search/', views.search_results, name='search_results'),
+    path('delete/', views.delete_all_data, name='delete_all_data'),
+
 ]
